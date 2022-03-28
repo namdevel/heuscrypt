4eff7cd88310ce66f23ea65c737df43d2d5b221f
几句レし <62225978+namdevel@users.noreply.github.com>
2022-03-28 16:42:43 +0700

    Create file_encrypted.py

---
+++ file_encrypted.py
@@ -0,0 +1,4 @@
+from HEUSCrypt import HEUSCryptObfuscator
+# Obfuscated using HEUSCrypt (https://www.github.com/namdevel)
+
+HEUSCryptObfuscator().loads(__file__); ''' HEUSCrypt_Payload:::eyJoZXVzY3J5cHRfaGFz_H/E/U/S/C/R/Y/P/T_aCI6ICJXdDFYQW5UWWZW_H/E/U/S/C/R/Y/P/T_eUNqUnhJYmJCZ21nPT0i_H/E/U/S/C/R/Y/P/T_LCAiaGV1c2NyeXB0X3Bh_H/E/U/S/C/R/Y/P/T_eWxvYWQiOiAiSlpFVWE2_H/E/U/S/C/R/Y/P/T_ZHZFWm1yM0FmaWJwQkxy_H/E/U/S/C/R/Y/P/T_UzV6VHFpa2JDMnpPRlNY_H/E/U/S/C/R/Y/P/T_QW0wbElKUk41aUo2STRE_H/E/U/S/C/R/Y/P/T_SGdtRk1QeUgvejJEcSJ9@@@ '''
